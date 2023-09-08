698ed56f616c0927a945a21623782b78a4164059
rebelraelin <cayla.raelin@gmail.com>
2023-09-08 15:52:16 -0700

    tells user mistake in alert

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -76,7 +76,7 @@ struct ContentView: View {
             scoreTitle = "Correct"
             score += 1
         } else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Incorrect! That's the flag of \(countries[number])"
         }
         
         showingScore = true
