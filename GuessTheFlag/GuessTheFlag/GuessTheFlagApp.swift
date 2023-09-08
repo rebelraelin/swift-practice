5ea0b1a96594cdd4b661e9c98ae8fb4468657288
rebelraelin <cayla.raelin@gmail.com>
2023-09-07 18:44:04 -0700

    guess the flag project

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Cayla Murdoch on 9/7/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
