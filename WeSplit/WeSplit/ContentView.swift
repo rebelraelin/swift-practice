ae217b12b70231e3570e0782e83a1dc849560ec8
rebelraelin <cayla.raelin@gmail.com>
2023-09-10 15:32:35 -0700

    0% tip has red text

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -63,6 +63,7 @@ struct ContentView: View {
                 
                 Section {
                     Text(totalAmount, format: .currency(code: currencyCode))
+                        .foregroundColor(tipPercentage == 0 ? .red : .primary)
                 } header: {
                     Text("Total Amount")
                 }
