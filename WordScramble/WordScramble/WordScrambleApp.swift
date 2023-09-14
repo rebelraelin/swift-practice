d2507ada898099d0736de3b898082e6b7c756b11
rebelraelin <cayla.raelin@gmail.com>
2023-09-14 16:23:25 -0700

    word scramble project

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Cayla Murdoch on 9/14/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
