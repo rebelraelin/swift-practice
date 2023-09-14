968da099efc8236b31546e074be3e6758c33840b
rebelraelin <cayla.raelin@gmail.com>
2023-09-14 11:48:12 -0700

    BetterRest project added ml model and logic

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Cayla Murdoch on 9/13/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
