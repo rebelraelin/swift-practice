6038656198d387b1788b85d4633656209701ed47
rebelraelin <cayla.raelin@gmail.com>
2023-10-14 15:33:39 -0700

    animation proj w blurry button

---
+++ Animation/Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Cayla Murdoch on 10/4/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animation/Animation/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Animation
+//
+//  Created by Cayla Murdoch on 10/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var animationAmount = 1.0
+    
+    var body: some View {
+        Button("Tap Me") {
+            animationAmount += 1
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .scaleEffect(animationAmount)
+        .blur(radius: (animationAmount - 1) * 3)
+        .animation(.default, value: animationAmount)
+    }
+}
+
+#Preview {
+    ContentView()
+}
